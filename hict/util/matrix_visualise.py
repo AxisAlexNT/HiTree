82cbf755746f6ec1f4690cdfa78b8367d69a5075
vitdrav <vitdrav@gmail.com>
2023-01-24 15:20:36 +0300

    Add diverging colormap

--- hict/util/matrix_visualise.py
+++ hict/util/matrix_visualise.py
@@ -4,7 +4,7 @@ from hict.api.ContactMatrixFacet import ContactMatrixFacet
 from hict.core.chunked_file import ChunkedFile
 import matplotlib.colors as clr
 
-from HiCT_Library.hict.core.common import ScaffoldDescriptor
+from hict.core.common import ScaffoldDescriptor
 
 
 class MatrixVisualise(object):
@@ -24,6 +24,10 @@ class MatrixVisualise(object):
 
         if weighted:
             matrix = ContactMatrixFacet.apply_cooler_balance_to_dense_matrix(matrix, w_r, w_c, False)
+
+        matrix = matrix.astype('float32')
+        matrix[matrix == 0] = np.nan
+
         return matrix
 
     @staticmethod
@@ -41,6 +45,18 @@ class MatrixVisualise(object):
         return clr.LinearSegmentedColormap.from_list('custom colormap', [(gradient_levels[0], start_color_hex),
                                                                          (gradient_levels[1], mid_color_hex),
                                                                          (gradient_levels[2], end_color_hex)])
+
+    @staticmethod
+    def get_colormap_diverging(first_quarter: float = 0.250, second_quarter: float = 0.750,
+                               start_color:tuple = (0.000, 0.145, 0.702), end_color:tuple =  (0.780, 0.012, 0.051),
+                               mid_color:tuple =  (1.000, 1.000, 1.000) ) \
+            -> clr.LinearSegmentedColormap:
+        return clr.LinearSegmentedColormap.from_list('diverging_clr', (
+            (0.000, start_color),
+            (first_quarter, start_color),
+            (0.500,mid_color),
+            (second_quarter, end_color),
+            (1.000, end_color)))
 
     @staticmethod
     def calculate_diag_means(matrix: np.ndarray, scaffold_first: ScaffoldDescriptor,
@@ -61,7 +77,7 @@ class MatrixVisualise(object):
                     expected[i, j] = averages_at_dist[abs(i - j)]
 
         if res == 'exp/obs':
-            return expected / matrix
+            return expected/matrix
 
         if res == 'exp':
             return expected
@@ -73,6 +89,6 @@ class MatrixVisualise(object):
             return matrix - expected
 
         if res == 'obs/exp':
-            return matrix / expected
+            return matrix/expected
 
         return result
