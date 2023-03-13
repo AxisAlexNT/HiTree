3b0f36be08614c0ff00d7c3708007c6f1dd1fc09
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-03-13 17:11:45 +0300

    split added

--- hict/core/scaffold_tree.py
+++ hict/core/scaffold_tree.py
@@ -1,4 +1,5 @@
 from hict.core.common import ScaffoldDescriptor
+from readerwriterlock import rwlock
 
 
 class ScaffoldTree(object):
@@ -49,11 +50,12 @@ class ScaffoldTree(object):
 
         def push(self) -> 'ScaffoldTree.Node':
             new_node = self.clone()
-            if self.needs_changing_direction:
+            if new_node.needs_changing_direction:
                 (new_node.left, new_node.right) = (
                     new_node.right,
                     new_node.left
                 )
+            return new_node
 
         def update_sizes(self) -> 'ScaffoldTree.Node':
             new_node = self.clone()
@@ -64,11 +66,57 @@ class ScaffoldTree(object):
                 new_node.subtree_length_bp += new_node.right.subtree_length_bp
             return new_node
 
+        @staticmethod
+        def split_bp(t: Optional['ScaffoldTree.Node'], left_size: np.int64, include_equal_to_the_left: bool) -> Tuple[Optional['ScaffoldTree.Node'], Optional['ScaffoldTree.Node']]:
+            if t is None:
+                return None
+            new_t = t.push()
+            left_subtree_length = (
+                new_t.left.subtree_length_bp
+                if new_t.left is not None else np.int64(0)
+            )
+            if left_size <= left_subtree_length:
+                (t1, t2) = ScaffoldTree.Node.split_bp(
+                    new_t.left,
+                    left_size,
+                    include_equal_to_the_left
+                )
+                new_t.left = t2
+                new_new_t = new_t.push()
+                return (t1, new_new_t)
+            elif left_subtree_length < left_size <= left_subtree_length + new_t.length_bp:
+                if new_t.scaffold_descriptor is not None:
+                    if include_equal_to_the_left:
+                        t2 = new_t.right
+                        new_t.right = None
+                        return new_t.update_sizes(), t2
+                    else:
+                        t1 = new_t.left
+                        new_t.left = None
+                        return t1, new_t.update_sizes()
+                else:
+                    t1 = new_t.clone()
+                    t2 = new_t.clone()
+                    t1.length_bp = left_size - left_subtree_length
+                    t2.length_bp -= t1.length_bp
+                    t1.right = None
+                    t2.left = None
+                    return t1.update_sizes(), t2.update_sizes()
+            else:
+                (t1, t2) = ScaffoldTree.Node.split_bp(
+                    new_t.right,
+                    left_size - left_subtree_length - new_t.length_bp,
+                    include_equal_to_the_left
+                )
+            pass
+
     root: ScaffoldTree.Node
+    root_lock: rwlock.RWLockWrite
 
     def __init__(
-            self,
-            assembly_length_bp: np.int64
+        self,
+        assembly_length_bp: np.int64,
+        mp_manager: Optional[multiprocessing.managers.SyncManager] = None
     ):
         self.root = ScaffoldTree.Node(
             length_bp=assembly_length_bp,
@@ -82,3 +130,8 @@ class ScaffoldTree(object):
             left=None,
             right=None
         )
+        if mp_manager is not None:
+            lock_factory = mp_manager.RLock
+        else:
+            lock_factory = threading.RLock
+        self.root_lock = rwlock.RWLockWrite(lock_factory=lock_factory)
