d94213619566c17ddf0702161b9fabe7b08e6045
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-05-11 18:03:40 +0300

    Fixed requirements and module discovery

--- setup.py
+++ setup.py
@@ -1,31 +1,25 @@
-from setuptools import setup
+from typing import List
+from setuptools import find_packages, setup
+
+
+requirements: List[str] = []
+with open("requirements.txt", mode="rt", encoding="utf-8") as f:
+    requirements = f.readlines()
 
 setup(
     name='hict',
     version='0.1.1rc1',
-    packages=['hict', 'hict.api', 'hict.core', 'hict.util'],
+    packages=list(set(['hict', 'hict.api', 'hict.core', 'hict.util']).union(find_packages())),
     url='https://genome.ifmo.ru',
     license='',
     author='Alexander Serdiukov, Anton Zamyatin and CT Lab ITMO University team',
     author_email='',
     description='HiCT is a model for efficient interaction with Hi-C contact matrices that actively uses Split-Merge tree structures.',
     setup_requires=[
-        'setuptools~=63.2.0',
-        'wheel~=0.37.1',
+        'setuptools>=63.2.0',
+        'wheel>=0.37.1',
     ],
-    install_requires=[
-        'h5py >= 3.7.0, < 3.8',
-        'matplotlib >= 3.5.2, <3.7',
-        'recordclass >= 0.17.2, < 0.19',
-        'frozendict>=2.3.4, < 2.4',
-        'scipy >=1.8.1, <1.10',
-        'numpy >= 1.23.2, < 1.25',
-        'cachetools>=5.2.0, <5.3',
-        'bio>=1.3.9, < 1.6',
-        'readerwriterlock>=1.0.9, < 1.1',
-        'setuptools>=63.2.0, < 66',
-        'wheel>=0.37.1, < 0.39',
-    ],
+    install_requires=list(set([]).union(requirements)),
     tests_require=[
         'cooler >=0.8.11, <0.9',
         'pytest >=7.2, <8',
